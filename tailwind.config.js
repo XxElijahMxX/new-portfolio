2952cadc88e8941df256c2fd52d9409913b3bf0f
Sheridan Melton <99241310+XxElijahMxX@users.noreply.github.com>
2023-03-04 22:21:13 -0500

    updated tailwind config

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,14 +1,16 @@
 /** @type {import('tailwindcss').Config} */
-export const darkMode = 'class';
-export const content = [
-  "./app/**/*.{js,ts,jsx,tsx}",
-  "./pages/**/*.{js,ts,jsx,tsx}",
-  "./components/**/*.{js,ts,jsx,tsx}",
-
-  // Or if using `src` directory:
-  "./src/**/*.{js,ts,jsx,tsx}",
-];
-export const theme = {
-  extend: {}
-};
-export const plugins = [];
+module.exports = {
+  darkMode: 'class',
+  content: [
+    "./app/**/*.{js,ts,jsx,tsx}",
+    "./pages/**/*.{js,ts,jsx,tsx}",
+    "./components/**/*.{js,ts,jsx,tsx}",
+ 
+    // Or if using `src` directory:
+    "./src/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
